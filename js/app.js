90d868a7bc24947c4bcb032b8d661db688a89dbc
Alexandre Blanquart <alex.blanquart@gmail.com>
2015-08-31 16:22:54 +0200

    starting to go jekyll way, easier for a non-programmer to use

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(function() {
+
+	imagesLoaded('.grid', function(grid) {
+		var msnry = new Masonry('.grid', {itemSelector: '.grid-item', columnWidth: 1});
+	});	
+
+	$('.description').on('show.bs.collapse', function () {
+		// scroll to top to see content
+	  	$('body').animate({scrollTop: 0},'slow');
+	});
+
+});
+
