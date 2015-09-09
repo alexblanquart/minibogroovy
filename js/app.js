7a4f14bbcd59b566a582b173950e43cd7d38cbde
Alexandre Blanquart <alex.blanquart@gmail.com>
2015-09-09 10:50:44 +0200

    added navbar active links

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(function() {
 
+	// mansory grid layout
 	imagesLoaded('.grid', function(grid) {
 		var msnry = new Masonry('.grid', {itemSelector: '.grid-item', columnWidth: 1});
 	});	
@@ -8,6 +9,5 @@ $(function() {
 		// scroll to top to see content
 	  	$('body').animate({scrollTop: 0},'slow');
 	});
-
 });
 
