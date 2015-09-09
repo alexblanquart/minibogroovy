22c1c565edba0e882168ecae557d910a08407b2d
Alexandre Blanquart <alex.blanquart@gmail.com>
2015-09-09 16:40:40 +0200

    masonry and infite scrolling on posts

--- js/app.js
+++ js/app.js
@@ -1,9 +1,36 @@
 $(function() {
 
-	// mansory grid layout
-	imagesLoaded('.grid', function(grid) {
-		var msnry = new Masonry('.grid', {itemSelector: '.grid-item', columnWidth: 1});
-	});	
+	// initialize masonry
+	var $container = $('.grid');
+	$container.imagesLoaded(function(){
+      $container.masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 1
+      });
+    });
+
+	// use infinite scroll coupled to jekyll pagination
+    $container.infinitescroll({
+      navSelector  : '.pagination',    // selector for the paged navigation 
+      nextSelector : '.pagination a',  // selector for the NEXT link : to next items to display
+      itemSelector : '.grid-item',     // selector for all items you'll retrieve
+      loading: {
+          finishedMsg: 'Plus rien à charger.',
+          img: 'http://i.imgur.com/6RMhx.gif'
+        }
+      },
+      // trigger Masonry as a callback
+      function( newElements ) {
+        // hide new items while they are loading
+        var $newElems = $( newElements ).css({ opacity: 0 });
+        // ensure that images load before adding to masonry layout
+        $newElems.imagesLoaded(function(){
+          // show elems now they're ready
+          $newElems.animate({ opacity: 1 });
+          $container.masonry( 'appended', $newElems, true ); 
+        });
+      }
+    );
 
 	$('.description').on('show.bs.collapse', function () {
 		// scroll to top to see content
